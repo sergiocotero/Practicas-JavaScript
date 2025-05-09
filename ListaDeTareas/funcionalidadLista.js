44b97ed895e485e6b9e073abb8ae70ba92b976b3
sergiocotero <sergio.cotero@uabc.edu.mx>
2025-05-09 12:19:37 -0700

    Primer commit, subo ListaDeTareas base, sin diseño pero funcional

---
+++ ListaDeTareas/funcionalidadLista.js
@@ -0,0 +1,26 @@
+function agregarTarea(){
+    let nuevaTarea = document.getElementById("nuevaTarea").value
+
+    //Validamos que no este vacío el texto ingresado
+    if(nuevaTarea == ""){
+        alert("Debe de agregar una tarea")
+        return
+    }
+
+    //Creo un nuevo elemento en mi lista
+    let tarea = document.createElement("li")
+    tarea.textContent = nuevaTarea
+
+    let botonEliminar = document.createElement("button")
+    botonEliminar.textContent = "Eliminar"
+    botonEliminar.onclick = function(){ tarea.remove() }
+
+    //Vincula el botón eliminar con el nuevo elemento
+    tarea.appendChild(botonEliminar)
+
+    //Agrega el elemento
+    document.getElementById("listaTarea").appendChild(tarea)
+
+    //Vacía el input
+    document.getElementById("nuevaTarea").value = ""
+}
